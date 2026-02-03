04c334693e6153aae28035dec70673a220dc0b68
joao tiago viegas <3536754+jtviegas@users.noreply.github.com>
2026-02-03 23:02:30 +0100

    wip

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # datasets
 handy datasets
+
+
+![Coverage](./coverage.svg)
+[![PyPI](https://img.shields.io/pypi/v/tgedr-dataset)](https://pypi.org/project/tgedr-dataset/)
+
+
+
+## development
+- main requirements:
+  - _uv_  
+  - _bash_
+- Clone the repository like this:
+
+  ``` bash
+  git clone git@github.com:jtviegas/datasets
+  ```
+- cd into the folder: `cd datasets`
+- install requirements: `./helper.sh reqs`
--- helper.sh
+++ helper.sh
@@ -95,7 +95,7 @@ reqs(){
   _pwd=`pwd`
   cd "$this_folder"
 
-  assert_uv_config
+  assert_uv_config &&  uv sync --group dev
   local result="$?"
 
   cd "$_pwd"
@@ -104,7 +104,194 @@ reqs(){
   info "$msg"
 }
 
+
+unit_test(){
+  info "[unit_test|in] ($1)"
+
+  local FOLDER=$TEST_DIR
+  [[ ! -z "$1" ]] && FOLDER="$1"
+
+  _pwd=`pwd`
+  cd "$this_folder"
+
+  uv run pytest "$FOLDER" -x -s -vv --durations=0 \
+    --cov="$SRC_DIR" \
+    --cov-report=term-missing \
+    --cov-report=html \
+    --cov-report=xml \
+    --junitxml=unit-tests-results.xml
+  local result="$?"
+  [[ ! "$result" -eq "0" ]] && err "[unit_test] tests failed"
+  cd "$_pwd"
+
+  local msg="[unit_test|out] => ${result}"
+  [[ ! "$result" -eq "0" ]] && info "$msg" && exit 1
+  info "$msg"
+}
+
+unit_test_print_coverage()
+{
+  info "[unit_test_print_coverage|in]"
+  
+  uv run coverage report --show-missing
+  uv run coverage html
+  uv run coverage xml
+  result="$?"
+  [ "$result" -ne "0" ] && exit 1
+  info "[unit_test_print_coverage|out] => $result"
+  return ${result}
+}
+
+unit_test_coverage_check()
+{
+  info "[unit_test_coverage_check|in] ($1)"
+  [ -z "$1" ] && usage
+
+  local threshold=$1
+  score=$(uv run coverage report | awk '$1 == "TOTAL" {print $NF+0}')
+  result="$?"
+  [ "$result" -ne "0" ] && exit 1
+  if (( $threshold > $score )); then
+    err "[unit_test_coverage_check] $score doesn't meet $threshold"
+    exit 1
+  fi
+  uv run genbadge coverage -i coverage.xml -o coverage.svg
+  info "[unit_test_coverage_check|out] => $score"
+}
+
+build(){
+  info "[build|in]"
+
+  _pwd=`pwd`
+  cd "$this_folder"
+  # changelog
+  rm -rf dist/*
+  uv build
+  local result="$?"
+  [[ ! "$result" -eq "0" ]] && err "[build] build failed"
+
+  cd "$_pwd"
+  local msg="[build|out] => ${result}"
+  [[ ! "$result" -eq "0" ]] && info "$msg" && exit 1
+  info "$msg"
+}
+
+publish(){
+  info "[publish|in]"
+
+  _pwd=`pwd`
+  cd "$this_folder"
+
+  uv publish --token "$PYPI_TOKEN"
+  local result="$?"
+  [[ ! "$result" -eq "0" ]] && err "[publish] publish failed"
+
+  cd "$_pwd"
+  local msg="[publish|out] => ${result}"
+  [[ ! "$result" -eq "0" ]] && info "$msg" && exit 1
+  info "$msg"
+}
+
+sca_check_safety(){
+  info "[sca_check_safety|in] (${1:0:3})"
+  _pwd=`pwd`
+
+  [ -z $1 ] && err "[sca_check_safety] missing argument SAFETY_KEY" && exit 1
+  local SAFETY_KEY="$1"
+
+  local result=0
+  cd "$this_folder"
+
+  # Run safety scan with continue-on-error flag
+  uv run safety --key "$SAFETY_KEY" scan --detailed-output --continue-on-error || true
+  actual_result="$?"
+  info "[sca_check_safety] actual exit code: $actual_result"
+  
+  # Exit codes: 0=success, 64=vulnerabilities found, 68=policy violation
+  if [ "$actual_result" -eq "64" ] || [ "$actual_result" -eq "68" ]; then
+    warn "[sca_check_safety] vulnerabilities or policy violations found (exit code: $actual_result)"
+    result=0  # Don't fail CI on vulnerabilities for now
+  elif [ "$actual_result" -ne "0" ]; then
+    warn "[sca_check_safety] scan failed with exit code: $actual_result"
+    result=0  # Don't fail CI
+  else
+    result=0
+  fi
+  
+  info "[sca_check_safety] scan completed with exit code: $actual_result (treating as: $result)"
+  cd "$_pwd"
+
+  local msg="[sca_check_safety|out] => ${result}"
+  [[ ! "$result" -eq "0" ]] && info "$msg" && exit 1
+  info "$msg"
+}
+
+sast_check_bandit(){
+  info "[sast_check_bandit|in] ($1)"
+  _pwd=`pwd`
+
+  [ -z $1 ] && err "[sast_check_bandit] missing argument SRC_DIR" && exit 1
+  local SRC_DIR="$1"
+
+  cd "$this_folder"
+
+  uv run bandit -r $SRC_DIR
+  local result="$?"
+  if [ ! "$result" -eq "0" ] ; then err "[sast_check_bandit] code check had issues"; fi
+
+  cd "$_pwd"
+
+  local msg="[sast_check_bandit|out] => ${result}"
+  [[ ! "$result" -eq "0" ]] && info "$msg" && exit 1
+  info "$msg"
+}
+
+lint_check_ruff(){
+  info "[lint_check_ruff|in]"
+  _pwd=`pwd`
+
+  cd "$this_folder"
+
+  uv run ruff check
+  local result="$?"
+  if [ ! "$result" -eq "0" ] ; then err "[lint_check_ruff] ruff linter check had issues"; fi
+
+  cd "$_pwd"
+
+  local msg="[lint_check_ruff|out] => ${result}"
+  [[ ! "$result" -eq "0" ]] && info "$msg" && exit 1
+  info "$msg"
+}
+
+git_tag_and_push()
+{
+  info "[git_tag_and_push|in] ($1, ${2:0:7})"
+
+  [ -z "$1" ] && err "must provide parameter VERSION" && exit 1
+  local VERSION="$1"
+  [ -z "$2" ] && err "must provide parameter COMMIT_HASH" && exit 1
+  local COMMIT_HASH="$2"
+
+  git tag -a "$VERSION" "$COMMIT_HASH" -m "release $VERSION" && git push --tags
+  result="$?"
+  [ "$result" -ne "0" ] && err "[git_tag_and_push|out] could not tag and push" && exit 1
+
+  info "[git_tag_and_push|out] => ${result}"
+}
+
+get_latest_tag() {
+  info "[get_latest_tag|in]"
+  git fetch --tags > /dev/null 2>&1
+  latest_tag=$(git describe --tags --abbrev=0 2>/dev/null)
+  local result=0
+  if [ ! -z "$latest_tag" ]; then
+      result="$latest_tag"
+  fi
+  info "[get_latest_tag|out] => ${result}"
+}
+
 # <=== MAIN SECTION END  <===
+
 
 # ===> FOOTER SECTION START  ===>
 
@@ -113,26 +300,67 @@ usage() {
   usage:
   $(basename $0) { option }
     options:
-      - reqs        checks requirements
-      - print_uuid  prints a new uuid
+      - install_qa_libs                  installs QA requirements (bandit, safety)
+      - uninstall_qa_libs                uninstalls QA requirements (bandit, safety)
+      - reqs                              installs development requirements
+      - linter_check                      runs code lint and format check
+      - sast_check                        runs static application security tests (SAST) check
+      - sca_check                         runs software component analysis (SCA) check
+      - test [<test_folder>]              runs unit tests
+      - test_coverage                     prints test coverage report
+      - test_coverage_check <threshold>   checks coverage against a threshold
+      - build                             builds the package
+      - publish                           publishes the package
+      - tag <VERSION> <COMMIT_HASH>       tags a specific commit with the version and pushes it to the remote
+      - get_latest_tag
 EOM
   exit 1
 }
 
-# -------------------------------------
 
 case "$1" in
+  install_qa_libs)
+    install_qa_libs
+    ;;
+  uninstall_qa_libs)
+    uninstall_qa_libs
+    ;;
   reqs)
     reqs
     ;;
-  print_uuid)
-    print_uuid
+  linter_check)
+    lint_check_ruff
+    ;;
+  sast_check)
+    sast_check_bandit "$this_folder/src"
+    ;;
+  sca_check)
+    sca_check_safety "$SAFETY_KEY"
+    ;;
+  test)
+    unit_test "$2"
+    ;;
+  test_coverage)
+    unit_test_print_coverage
+    ;;
+  test_coverage_check)
+    unit_test_coverage_check "$2"
+    ;;
+  build)
+    build
+    ;;
+  publish)
+    publish
+    ;;
+  tag)
+    git_tag_and_push "$2" "$3"
+    ;;
+  get_latest_tag)
+    get_latest_tag
     ;;
   *)
     usage
     ;;
 esac
 
-
 # <=== FOOTER SECTION END  <===
-
